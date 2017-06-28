7376dabb9b02d74adca7daf998f99165f63e94f3
betinbeton <acortes00@hotmail.com>
2017-06-27 22:22:01 -0500

    Add files via upload

---
+++ PreciosGas/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Android.Content;
+
+namespace PreciosGas
+{
+    [Activity(Label = "PreciosGas", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        public static Context context;
+        public static List<UserInfo> UserInfoList = new List<UserInfo>();
+        public static ListView ListView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+            ListView = FindViewById<ListView>(Resource.Id.listView);
+            GetList list = new GetList();
+            list.Execute();
+            
+        }
+
+
+        public class GetList : AsyncTask
+        {
+       
+            Context con;
+            protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params)
+            {
+                //toma la informacion
+                System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+                var _WebApiUrl = string.Format("https://URL/api/Account/Profile");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage messge = client.GetAsync(_WebApiUrl).Result;
+                var Return_EventList = messge.Content.ReadAsStringAsync().Result;
+                var EventList = JsonConvert.DeserializeObject<List<UserInfo>>(Return_EventList);
+                foreach (var data in EventList)
+                {
+                
+                   UserInfoList.Add(data);
+            
+                }
+                return true;
+            }
+
+
+            protected override void OnPreExecute()
+            {
+                base.OnPreExecute();
+            }
+            protected override void OnPostExecute(Java.Lang.Object result)
+            {
+                base.OnPostExecute(result);
+           
+                ListView.Adapter = new UserInfoListAdapter(context, UserInfoList);
+            }
+        }
+    }
+}
+
---
+++ PreciosGas/UserInfoListAdapter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Views;
+using Android.Widget;
+using Android.Content;
+
+namespace PreciosGas
+{
+    class UserInfoListAdapter : BaseAdapter<UserInfo>
+    {
+        private List<UserInfo> mItem = new List<UserInfo>();
+        private Context context;
+
+        
+        public UserInfoListAdapter(Context mcontext, List<UserInfo> mItems)
+        {
+            mItem.Clear();
+            mItem = mItems;
+            context = mcontext;
+            this.NotifyDataSetChanged();
+        }
+        
+
+        public override UserInfo this[int position]
+        {
+            get
+            {
+                return mItem[position];
+            }
+        }
+           
+
+        public override int Count
+        {
+            get
+            {
+                return mItem.Count;
+            }
+        }
+
+        public Context MContext
+        {
+            get;
+            private set;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View listitem = convertView;
+            listitem = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.ListViewDesign, parent, false);
+            TextView TxtName = listitem.FindViewById<TextView>(Resource.Id.TxtName);
+            TextView TxtNumber = listitem.FindViewById<TextView>(Resource.Id.TxtNumber);
+            TxtName.Text = mItem[position].firstname;
+            TxtNumber.Text = mItem[position].contact_no;
+            listitem.Click += (object sender, EventArgs e) => {
+                Toast.MakeText(parent.Context, "Clicked " + mItem[position].firstname, ToastLength.Long).Show();
+            };
+            return listitem;
+        }
+    }
+}
